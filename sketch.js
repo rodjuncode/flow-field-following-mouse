cc07c0ba4c588011e6a490ca4646cd59e0d1c22a
Rodrigo Junqueira <rodrigo.fpjunqueira@gmail.com>
2020-12-24 12:12:25 -0300

    Added delay to start to fix corner activation.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let gridX = 60;
 let gridY = 30;
 let radius = 200;
+let startDelay = 5;
+let frame = 0;
 let cellWidth;
 let cellHeight;
 let field;
@@ -20,14 +22,17 @@ function setup() {
 }
 
 function draw() {
-    background(255);
+    background(225,0,255);
+    noFill();
+    stroke(255,255,0);
+    strokeWeight(2);
     for (let i = 0; i < gridX; i++) {
         for (let j = 0; j < gridY; j++) {
             push();
             let pos = createVector(i*cellWidth,j*cellHeight);
             let ref = rotationReference();
             let abs = createVector(pos.x + ref.x, pos.y + ref.y);
-            if (abs.dist(createVector(mouseX,mouseY)) < radius) {
+            if (abs.dist(createVector(mouseX,mouseY)) < radius && frame > startDelay) {
                 let angle = atan2(mouseY - (abs.y), mouseX - (abs.x));
                 field[i][j] = angle;
             }
@@ -38,6 +43,7 @@ function draw() {
             pop();
         }    
     }   
+    frame++;
 }
 
 function rotationReference() {
